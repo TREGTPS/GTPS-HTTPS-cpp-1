860f618a3a6a6a462aae47416a47814d37276c26
Lukats_ <53633071+LukatsEE@users.noreply.github.com>
2022-06-29 18:36:29 +0300

    fmt

---
+++ packages/fmt/test/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/acer/Desktop/GrowXYZ/GrowXYZ/vendor/fmt/test/gtest
+# Build directory: C:/Users/acer/Desktop/GrowXYZ/GrowXYZ/vendor/fmt/test/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
